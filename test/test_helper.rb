486a7a07c18ea1c2e6f2921e5ec7673f1ea34abf
rikupoo <nemutir@gmail.com>
2022-09-16 06:30:12 +0900

    test_environment_setup

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -2,7 +2,17 @@ ENV['RAILS_ENV'] ||= 'test'
 require_relative '../config/environment'
 require 'rails/test_help'
 
+# gem 'minitest-reporters' セットアップ
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 class ActiveSupport::TestCase
+
+  #seed.rbを読み込みテスト環境でもseedデータを使用可にする
+  parallelize_setup do |worker|
+    load "#{Rails.root}/db/seeds.rb"
+  end
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
