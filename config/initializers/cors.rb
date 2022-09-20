2171503197288bc046b37166f7fbe871ca84c44f
rikupoo <nemutir@gmail.com>
2022-09-21 06:34:04 +0900

    add_credentials_to_cors

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -11,6 +11,8 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
 
     resource '*',
       headers: :any,
-      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+      methods: [:get, :post, :put, :patch, :delete, :options, :head],
+      # クロスオリジンのCookieを使用するための設定
+      credentials: true
   end
 end
